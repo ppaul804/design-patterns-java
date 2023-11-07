8664c6029c6e5c2e57057904573b65aebb296f67
Pedro Paul <pedro.paul@cassi.com.br>
2023-11-06 23:27:22 -0300

    Decorator

---
+++ src/main/java/br/com/alura/TestesImposto.java
@@ -0,0 +1,18 @@
+package br.com.alura;
+
+import br.com.alura.loja.imposto.CalculadoraDeImpostos;
+import br.com.alura.loja.imposto.ICMS;
+import br.com.alura.loja.imposto.ISS;
+import br.com.alura.loja.orcamento.Orcamento;
+import lombok.extern.slf4j.Slf4j;
+
+import java.math.BigDecimal;
+
+@Slf4j
+public class TestesImposto {
+    public static void main(String[] args) {
+        Orcamento orcamento = new Orcamento(BigDecimal.valueOf(100), 1);
+        CalculadoraDeImpostos calculadora = new CalculadoraDeImpostos();
+        log.info("{}", calculadora.calcular(orcamento, new ISS(new ICMS(null))));
+    }
+}
--- src/main/java/br/com/alura/loja/imposto/Imposto.java
+++ src/main/java/br/com/alura/loja/imposto/Imposto.java
@@ -1,11 +1,22 @@
 package br.com.alura.loja.imposto;
 
+import br.com.alura.loja.orcamento.Orcamento;
+import lombok.AllArgsConstructor;
+
 import java.math.BigDecimal;
 
-import br.com.alura.loja.orcamento.Orcamento;
+@AllArgsConstructor
+public abstract class Imposto {
+    private Imposto proximo;
 
-public interface Imposto {
+    protected abstract BigDecimal realizarCalculo(Orcamento orcamento);
 
-	BigDecimal calcular(Orcamento orcamento);
+    public BigDecimal calcular(Orcamento orcamento) {
+        BigDecimal valorImposto = realizarCalculo(orcamento);
+        BigDecimal valorDoProximoImposto = BigDecimal.ZERO;
+        if (proximo != null)
+            valorDoProximoImposto = proximo.realizarCalculo(orcamento);
+        return valorImposto.add(valorDoProximoImposto);
+    }
 
 }
